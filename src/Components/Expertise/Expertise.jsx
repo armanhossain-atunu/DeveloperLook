73040f45ae5b6c74512fcc8750c4fa438f033e69
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-04-18 23:57:20 +0600

    expertise complate

--- src/Components/Expertise/Expertise.jsx
+++ src/Components/Expertise/Expertise.jsx
@@ -14,16 +14,16 @@ const Expertise = () => {
             mousewheel={true}
             //   pagination={{ clickable: true }}
             modules={[Mousewheel, Pagination]}
-            className="mySwiper w-7xl mx-auto mt-20"
+            className="mySwiper mx-2 mt-20"
             style={{ height: '100vh' }}
         >
             <SwiperSlide>
-                <div className=" h-[500px] f bg-white rounded-4xl shadow-lg p-10">
+                <div className=" h-[500px]  bg-white rounded-4xl shadow-lg p-5">
                      <a href="#social-strategy" className="text-xl px-3 py-1 rounded-lg mb-10 font-bold bg-amber-200 ">Expertise</a>
-                    <div className="h-full flex lg:justify-between flex-col lg:flex-row gap-10">
+                    <div className="w-1/2 lg:w-full h-full flex lg:justify-between lg:flex-row gap-10">
                         <div className="lg:w-1/2">
-                            <a href="#social-strategy" className="text-6xl font-bold">social strategy </a>
-                            <p className="mt-5 text-2xl font-bold">
+                            <a href="#social-strategy" className="text-xl lg:text-6xl font-bold">social strategy </a>
+                            <p className="mt-5 lg:text-2xl lg:font-bold">
                                 <span>Slimme strategie. Sterke start.</span>
                                 We duiken diep in jouw merk, doelgroep en doelen. En vertalen data naar een duidelijk plan met formats die écht impact maken. Zo weet je precies waarom het werkt
                             </p>
@@ -31,8 +31,8 @@ const Expertise = () => {
                                 Leer ons kennen <span className="bg-black text-white rounded-lg p-3"><MoveRight /></span>
                             </button>
                         </div>
-                        <div className="lg:w-1/2 flex justify-end items-center">
-                            <div className="w-80 h-full bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
+                        <div className="lg:w-1/2 flex justify-end ">
+                            <div className="w-60 h-80 bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
                                 <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                                     <source src="https://gethyped.b-cdn.net/Petrol%20Head/petrolhead-loop.mp4" type="video/mp4" />
                                 </video>
@@ -42,12 +42,12 @@ const Expertise = () => {
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className=" h-[500px] f bg-[#fcb8fa] rounded-4xl shadow-lg p-10">
+                <div className=" h-[500px]  bg-[#fcb8fa] rounded-4xl shadow-lg p-5">
                      <a href="#social-strategy" className="text-xl px-3 py-1 rounded-lg mb-10 font-bold bg-amber-200 ">Expertise</a>
-                    <div className="h-full flex lg:justify-between flex-col lg:flex-row gap-10">
+                    <div className="w-1/2 lg:w-full h-full flex lg:justify-between lg:flex-row gap-10">
                         <div className="lg:w-1/2">
-                            <a href="#social-strategy" className="text-6xl font-bold">social strategy </a>
-                            <p className="mt-5 text-2xl font-bold">
+                            <a href="#social-strategy" className="text-xl lg:text-6xl font-bold">social strategy </a>
+                            <p className="mt-5 lg:text-2xl lg:font-bold">
                                 <span>Slimme strategie. Sterke start.</span>
                                 We duiken diep in jouw merk, doelgroep en doelen. En vertalen data naar een duidelijk plan met formats die écht impact maken. Zo weet je precies waarom het werkt
                             </p>
@@ -55,8 +55,8 @@ const Expertise = () => {
                                 Leer ons kennen <span className="bg-black text-white rounded-lg p-3"><MoveRight /></span>
                             </button>
                         </div>
-                        <div className="lg:w-1/2 flex justify-end items-center">
-                            <div className="w-80 h-full bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
+                        <div className="lg:w-1/2 flex justify-end">
+                            <div className="w-60 h-80 bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
                                 <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                                     <source src="https://gethyped.b-cdn.net/Petrol%20Head/petrolhead-loop.mp4" type="video/mp4" />
                                 </video>
@@ -66,12 +66,12 @@ const Expertise = () => {
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className=" h-[500px] f bg-white rounded-4xl shadow-lg p-10">
+                <div className=" h-[500px]  bg-white rounded-4xl shadow-lg p-5">
                      <a href="#social-strategy" className="text-xl px-3 py-1 rounded-lg mb-10 font-bold bg-amber-200 ">Expertise</a>
-                    <div className="h-full flex lg:justify-between flex-col lg:flex-row gap-10">
+                    <div className="w-1/2 lg:w-full h-full flex lg:justify-between lg:flex-row gap-10">
                         <div className="lg:w-1/2">
-                            <a href="#social-strategy" className="text-6xl font-bold">social strategy </a>
-                            <p className="mt-5 text-2xl font-bold">
+                            <a href="#social-strategy" className=" lg:text-6xl font-bold">social strategy </a>
+                            <p className="mt-5 lg:text-2xl lg:font-bold">
                                 <span>Slimme strategie. Sterke start.</span>
                                 We duiken diep in jouw merk, doelgroep en doelen. En vertalen data naar een duidelijk plan met formats die écht impact maken. Zo weet je precies waarom het werkt
                             </p>
@@ -80,7 +80,7 @@ const Expertise = () => {
                             </button>
                         </div>
                         <div className="lg:w-1/2 flex justify-end items-center">
-                            <div className="w-80 h-full bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
+                            <div className="w-60 h-80 bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
                                 <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                                     <source src="https://gethyped.b-cdn.net/Petrol%20Head/petrolhead-loop.mp4" type="video/mp4" />
                                 </video>
@@ -90,12 +90,12 @@ const Expertise = () => {
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className=" h-[500px] f bg-[#33c791] rounded-4xl shadow-lg p-10">
+                <div className=" h-[500px]  bg-[#33c791] rounded-4xl shadow-lg p-5">
                      <a href="#social-strategy" className="text-xl px-3 py-1 rounded-lg mb-10 font-bold bg-amber-200 ">Expertise</a>
-                    <div className="h-full flex lg:justify-between flex-col lg:flex-row gap-10">
+                    <div className="w-1/2 lg:w-full h-full flex lg:justify-between lg:flex-row gap-10">
                         <div className="lg:w-1/2">
-                            <a href="#social-strategy" className="text-6xl font-bold">social strategy </a>
-                            <p className="mt-5 text-2xl font-bold">
+                            <a href="#social-strategy" className="text-xl lg:text-6xl font-bold">social strategy </a>
+                            <p className="mt-5 lg:text-2xl lg:font-bold">
                                 <span>Slimme strategie. Sterke start.</span>
                                 We duiken diep in jouw merk, doelgroep en doelen. En vertalen data naar een duidelijk plan met formats die écht impact maken. Zo weet je precies waarom het werkt
                             </p>
@@ -103,8 +103,8 @@ const Expertise = () => {
                                 Leer ons kennen <span className="bg-black text-white rounded-lg p-3"><MoveRight /></span>
                             </button>
                         </div>
-                        <div className="lg:w-1/2 flex justify-end items-center">
-                            <div className="w-80 h-full bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
+                        <div className="lg:w-1/2 flex justify-end ">
+                            <div className="w-60 h-80 bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
                                 <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                                     <source src="https://gethyped.b-cdn.net/Petrol%20Head/petrolhead-loop.mp4" type="video/mp4" />
                                 </video>
@@ -114,12 +114,12 @@ const Expertise = () => {
                 </div>
             </SwiperSlide>
             <SwiperSlide>
-                <div className=" h-[500px] f bg-[#0d8dff] rounded-4xl shadow-lg p-10">
+                <div className=" h-[500px]  bg-[#0d8dff] rounded-4xl shadow-lg p-5">
                      <a href="#social-strategy" className="text-xl px-3 py-1 rounded-lg mb-10 font-bold bg-amber-200 ">Expertise</a>
-                    <div className="h-full flex lg:justify-between flex-col lg:flex-row gap-10">
+                    <div className="w-1/2 lg:w-full h-full flex lg:justify-between lg:flex-row gap-10">
                         <div className="lg:w-1/2">
-                            <a href="#social-strategy" className="text-6xl font-bold">social strategy </a>
-                            <p className="mt-5 text-2xl font-bold">
+                            <a href="#social-strategy" className="text-xl lg:text-6xl font-bold">social strategy </a>
+                            <p className="mt-5 lg:text-2xl lg:font-bold">
                                 <span>Slimme strategie. Sterke start.</span>
                                 We duiken diep in jouw merk, doelgroep en doelen. En vertalen data naar een duidelijk plan met formats die écht impact maken. Zo weet je precies waarom het werkt
                             </p>
@@ -127,8 +127,8 @@ const Expertise = () => {
                                 Leer ons kennen <span className="bg-black text-white rounded-lg p-3"><MoveRight /></span>
                             </button>
                         </div>
-                        <div className="lg:w-1/2 flex justify-end items-center">
-                            <div className="w-80 h-full bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
+                        <div className="lg:w-1/2 flex justify-end ">
+                            <div className="w-60 h-80 bg-[#fa5424] p-3 rounded-xl rotate-3 hover:rotate-0 transition-transform duration-300 overflow-hidden">
                                 <video className="w-full h-full object-cover" autoPlay loop muted playsInline>
                                     <source src="https://gethyped.b-cdn.net/Petrol%20Head/petrolhead-loop.mp4" type="video/mp4" />
                                 </video>
